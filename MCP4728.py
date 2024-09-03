d72a57a124c7d4f74cdc2b4189b2ff7c3a52452d
Isaac Kwabena Kyei Pappoe <kwabenapapps@gmail.com>
2024-09-03 00:03:39 -0400

    -Implement fast_write -Implement of set_channel -Update on channel, fast write and multi write

--- MCP4728.py
+++ MCP4728.py
@@ -6,8 +6,8 @@ from telemetrix import telemetrix
 _MCP4728_MULTI_IR_CMD = 0x40
 _MCP4728_ADDR = 0x60         # DEFAULT address
 _MCP4728_CMD_UPDATE = 0x00   # mcp4728 command for updating dac values???
-_MCP4728_CMD_EEPROM = 0xFF   # command for saving to eeprom   FIND THE VALUE??
-_MCP4728_MULTI_WRITE_CMD = 0x40
+_MCP4728_CMD_EEPROM = 0x50   # command for saving to eeprom   Is 50 the right value??
+_MCP4728_MULTI_WRITE_CMD = 0x40   # Don't know if this value is right??
 
 
 class MCP4728:
@@ -19,80 +19,105 @@ class MCP4728:
     def __init__(self, board, address: int = 0x60) -> None:         # DONE WITH THIS SECTION I GUESS??
         self.board = board
         self.addr = address    # moved this here
-
-        self.board.set_pin_mode_i2c()
-
-        # Focus here!!
-    def set_channel_value(self, channel: int, value: int, vref: int = 0, pd: int = 0) -> None:
-      high_byte = (channel << 4) | (value >> 8)
-      low_byte = value & 0xFF   # Lower bits
-
-    if not 0 <= channel <= 3:
-        ValueError("channel should be between 0 and 3")
-
-    if not 0 <= value <= 4095:
-        ValueError("value should be between 0 and 3")
-        command = [high_byte, low_byte]
-
-        self.board.i2c_write(self.addr, command)
-    print("Not implemented yet")
+        self.board.set_pin_mode_i2c()     # FOR I2C COMs
 
 
+        # SETTING CHANNELS AND SPLITTING BITS INTO HIGH AND LOW
+    def set_channel_value(self, channel: int, value: int, vref: int = 0, gain: int = 0, pd: int = 0, udac: int = 0) -> None:
+
+        # high_byte = (channel << 4) | (value >> 8)
+        # low_byte = value & 0xFF   # Lower bits
+        # command = [high_byte, low_byte]  # getting rid of this
+        command = bytearray(3)
+        command[0] = (_MCP4728_MULTI_WRITE_CMD | (channel << 1?) | udac?)
+        command[1] = ((value >> 8) & 0x0F)
+        command[2] = value & 0xFF
+
+      if not 0 <= channel <= 3:
+         raise ValueError("channel should be between 0 and 3")
+
+      if not 0 <= value <= 4095:                  # TO ENSURE CHANNEL IS WITHIN RANGE
+         raise ValueError("value should be between 0 and 4095")
+
+
+    self.board.i2c_write(self.addr, command)    # SAVED ON MEM
+    print("channel not complete yet")
 
 
     def fast_write(self, value_A: int, value_B: int, value_C: int, value_D: int) -> None:
         # Insert your code here
         # value  = [value_A, value_B, value_C, value_D]  # values for dac channels. right??
-        for value in [value_A, value_B, value_C, value_D]:
+        for value in [value_A, value_B, value_C, value_D]:    # for all channels
             if not 0 <= value <= 4095:
-                ValueError("All values should be within 0 and 4095")
+             raise ValueError("All values should be within 0 and 4095")   # to ensure within range. right?
         # print(f'Value D\n{value_A:016b}')
         # setting command for dac channels. Am I right??
-        command = [
+        # command = [
+        #
+        #     # CHANNEL A
+        #     (value_A >> 8) & 0xF, (value_A & 0xFF),
+        #     # CHANNEL B
+        #     (value_B >> 8) & 0xF, (value_B & 0xFF),
+        #     # CHANNEL C
+        #     (value_C >> 8) & 0xF, (value_C & 0xFF),
+        #     # CHANNEL D
+        #     (value_D >> 8) & 0xF, (value_D & 0xFF)
+        # ]
 
-            # CHANNEL A
-            (value_A >> 8) & 0xF, (value_A & 0xFF),
-            # CHANNEL B
-            (value_B >> 8) & 0xF, (value_B & 0xFF),
-            # CHANNEL C
-            (value_C >> 8) & 0xF, (value_C & 0xFF),
-            # CHANNEL D
-            (value_D >> 8) & 0xF, (value_D & 0xFF)
-        ]
-        # print(f'{command[0]:08b}{command[1]:08b}')
+        command = bytearray(9)
+        command[0] = (_MCP4728_MULTI_WRITE_CMD)
+        command[1] = (value_A >> 4) & 0xFF
+        command[2] = (value_A & 0x0F) << 4
+        command[3] = (value_B >> 4) & 0xFF
+        command[4] = (value_B & 0x0F) << 4
+        command[5] = (value_C >> 4) & 0xFF
+        command[6] = (value_C & 0x0F) << 4
+        command[7] = (value_D >> 4) & 0xFF
+        command[8] = (value_D & 0x0F) << 4
 
         self.board.i2c_write(self.addr, command)  # MOVED THIS HERE
         # command1 = _MCP4728_MULTI_IR_CMD
         # command.append(command1)
-        print("Not implemented yet")
+        print("fast write completed")
 
     def multi_write_dac(self, value_A: int, value_B: int, value_C: int, value_D: int) -> None:
-      command = [
+        for value in [value_A, value_B, value_C, value_D]:    # for all channels
+            if not 0 <= value <= 4095:
+                raise ValueError("for all values be within 0 and 4095")
 
-          _MCP4728_MULTI_WRITE_CMD,
-          # CHANNEL A
-          (value_A >> 8) & 0x0F, (value_A & 0xFF),
-          # CHANNEL B
-          (value_B >> 4) & 0xFF, (value_B & 0xFF),
-          # CHANNEL C
-          (value_C >> 4) & 0xFF, (value_C & 0xFF),
-          # CHANNEL D
-          (value_D >> 4) & 0xFF, (value_D & 0xFF)
-      ]
-      print(f'Value A\n{value_A:016b}')
+            command = bytearray(9)
+            command[0] = (_MCP4728_MULTI_WRITE_CMD)
+            command[1] = (value_A >> 4) & 0xFF
+            command[2] = (value_A & 0x0F) << 4
+            command[3] = (value_B >> 4) & 0xFF
+            command[4] = (value_B & 0x0F) << 4
+            command[5] = (value_C >> 4) & 0xFF
+            command[6] = (value_C & 0x0F) << 4
+            command[7] = (value_D >> 4) & 0xFF
+            command[8] = (value_D & 0x0F) << 4
+    # command = [
+    #
+    #       _MCP4728_MULTI_WRITE_CMD,
+    #       # CHANNEL A
+    #       (value_A >> 8) & 0x0F, (value_A & 0xFF),
+    #       # CHANNEL B
+    #       (value_B >> 4) & 0xFF, (value_B & 0xFF),
+    #       # CHANNEL C
+    #       (value_C >> 4) & 0xFF, (value_C & 0xFF),
+    #       # CHANNEL D
+    #       (value_D >> 4) & 0xFF, (value_D & 0xFF)
+    #     ]
 
-      print(f'{command[0]:08b}{command[1]:08b}')
-
-      self.board.i2c_write(self.addr, command)
-
+    self.board.i2c_write(self.addr, command)
+    print("multi write dac completed")
 
     # Extra credit, in case you want to try something new
-    #def set_channel_value(self, channel: int, value: int, new_vref: int, new_gain: int, new_pd_mode: int, udac: bool = 0) -> None:
+    # def set_channel_value(self, channel: int, value: int, new_vref: int, new_gain: int, new_pd_mode: int, udac: bool = 0) -> None:
         # print("Not implemented yet")
         # Insert code here
     def save_to_EEPROM(self) -> None:                               # DONE WITH THIS SECTION I GUESS??
         # I just put this here but don't know if it's write. This to save settings to eeprom
         command = [_MCP4728_CMD_EEPROM]                           # WHAT MIGHT BE THE ISSUE HERE???
         self.board.i2c_write(self.addr, command)
-        print("Not implemented yet")
+        print("save implement done")
         # Insert your code here
