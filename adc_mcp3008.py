9b2437aad17395abc10e262a1bd6b28fbbbf7da3
Isaac Kwabena Kyei Pappoe <kwabenapapps@gmail.com>
2024-11-26 12:30:45 -0500

    -Implement fast_write and pd -Implement of set_channel -Update on channel, fast write and multi write -Command setup, power down, gain and voltage reference setting ADC library Update adc

--- adc_mcp3008.py
+++ adc_mcp3008.py
@@ -17,10 +17,19 @@ class MCP3008DAC:
 
         self.board = board
         self.cs_pin = cs_pin
+        self.value = 0
 
         # Initializing spi here
         self.board.set_pin_mode_spi()
         self.board.set_pin_mode_digital_output(cs_pin)
+
+    def read_callback(self, report):
+        if len(report) >= 5:          # to ensure data is complete ??
+            msb = (report[3] & 0b00000011) << 8   # extract values from D9 and D8
+            lsb = report[4]                       # extract values from d7-d0
+            self.value = msb | lsb                # combining to get 10-bit value
+            self.value = report[4]
+            print("Callback ADC Value:, {self.value}")
 
     def read_adc(self, channel):
 
@@ -29,24 +38,23 @@ class MCP3008DAC:
 
         # constructing command byte for MCP3008 and setting start bit and diff mode to 1's
         command = 0b00011000 | (channel << 1)
-        #start_bit = 1
-        #end_bit = 1
-        #command = [start_bit, (end_bit << 7) | (channel << 4), 0]   # right??
 
         # chip select has to be low to start comm so setting CS to low = 0 to start comm
         self.board.digital_write(self.cs_pin, 0)
         time.sleep(0.001)     # for a bit of delay
 
         # changing this setting from spi transfer to spi blocking and setting up respective parameters
-        response = self.board.spi_read_blocking(register_selection=command, number_of_bytes_to_read=2, call_back=None,
+        response = self.board.spi_read_blocking(register_selection=command,number_of_bytes_to_read=2,call_back=self.read_callback,
                                                 enable_read_bit=False)
 
         # setting Chip Select high=1 to end comm
         self.board.digital_write(self.cs_pin, 1)
 
-        # setting the response to get the adc value 10-bit
+        # setting the response most and least significant bit to get the adc value 10-bit
         msb = (response[0] & 0b00000011) << 8   # to extract upper bits, D9 and D8
         lsb = response[1]                       # to extract lower bits, D7 to D0
         adc_value = msb | lsb                   # this to get the 10-bit
 
         return adc_value
+
+
